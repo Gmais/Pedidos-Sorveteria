1747c9174084e772958f9d04e7cd3e9f88354af7
Gilson <apcturvo@gmail.com>
2026-06-07 21:39:02 -0300

    fix: hardcode firebase config fallback

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,11 +3,11 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyATEZW_ALNBv92F-A9hbFwnFWpJDR69V9g",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "pedidos-sorveteria-1e33d.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "pedidos-sorveteria-1e33d",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "306306991395",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:306306991395:web:0e9b53d0ba03e76970f896",
 };
 
 export const app = initializeApp(firebaseConfig);
